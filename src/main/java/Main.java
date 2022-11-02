3fcaa5d04322c735b3aa3ce619feffbf9aa03d16
thienle77 <tle9677@gmail.com>
2022-11-02 15:42:28 -0400

    Progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        int num1, num2, sum = 0;
+        System.out.println("Please enter your numbers");
+        Scanner input = new Scanner (System.in);
+        num1 = input.nextInt();
+
+        for (int i  = 0; i <= num1; i++){
+            num2 = input.nextInt();
+            sum = num1 + num2;
+            System.out.println("The sum is " + sum);
+            break;
+        }
+        
     }
+
+
+
     
     int sumOfNumbers(int n) {
         return 0;
